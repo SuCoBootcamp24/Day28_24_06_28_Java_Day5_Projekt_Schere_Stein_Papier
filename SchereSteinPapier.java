b8b0b75a4f2ecf36e77436ddcc7c0c8877eff7b3
René Behrens <rene.behrens@aol.com>
2024-06-28 17:37:41 +0200

    "magic numbers" removed

--- SchereSteinPapier.java
+++ SchereSteinPapier.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class SchereSteinPapier {
@@ -7,6 +8,7 @@ public class SchereSteinPapier {
     private static final int PAPER = 2;
     private static final int GAMEEND = 3;
 
+    private static int counter = 0;
     private static int playerScore = 0;
     private static int compScore = 0;
 
@@ -16,7 +18,9 @@ public class SchereSteinPapier {
                 int menuSet = gameMenu(scanner);
                 if (menuSet == GAMEEND)
                     break;
-                versus(menuSet);
+
+                System.out.println("Deine Wahl: " + showSelectedObject(menuSet));
+                gameLogic(menuSet);
             }
 
             System.out.println("Spiel wurde beendet.\n");
@@ -26,15 +30,14 @@ public class SchereSteinPapier {
 
     private static int gameMenu(Scanner scanner) {
         System.out.println("\nWähle aus:");
-        System.out
-                .println(SHEARS + " = Schere, " + STONE + " = Stein, " + PAPER + " = Papier " + GAMEEND + " = Beenden");
+        System.out.println(SHEARS + " = Schere, " + STONE + " = Stein, " + PAPER + " = Papier " + GAMEEND + " = Beenden");
 
         int set = -1;
 
         while (true) {
             try {
                 set = scanner.nextInt();
-                if (set >= SHEARS && set <= GAMEEND) {
+                if (set == SHEARS || set == STONE || set == PAPER || set == GAMEEND) {
                     break;
                 } else {
                     System.out.println("Ungültige Spielereingabe");
@@ -48,38 +51,60 @@ public class SchereSteinPapier {
 
     }
 
-    private static int randomObject() {
-        return (int) (Math.random() * (3));
-    }
-
-    private static void versus(int playerObj) {
+    private static void gameLogic(int playerObj) {
         int compObj = randomObject();
 
+        System.out.println("Wahl des Computers: " + showSelectedObject(compObj));
+
         if (playerObj == compObj) {
-            System.out.println("\nuUentschieden   P: " + playerObj + " C: " + compObj);
+            System.out.println("\nUentschieden");
         } else if ((playerObj == SHEARS && compObj == PAPER) ||
                 (playerObj == STONE && compObj == SHEARS) ||
                 (playerObj == PAPER && compObj == STONE)) {
 
-            System.out.println("\nSpieler gewinnt   P: " + playerObj + " C: " + compObj);
+            System.out.println("\nSpieler gewinnt");
             playerScore++;
         } else {
-            System.out.println("\nComputer gewinnt   P: " + playerObj + " C: " + compObj);
+            System.out.println("\nComputer gewinnt");
             compScore++;
         }
+    }
+
+   
+    private static int randomObject() {
+        Random random = new Random();
+        int[] options = {SHEARS, STONE, PAPER};
+        return options[random.nextInt(options.length)];
+    }
+
+    private static String showSelectedObject(int obj) {
+        String objName = "";
+
+        switch (obj) {
+            case SHEARS:
+                objName = "Schere";
+                break;
+            case STONE:
+                objName = "Stein";
+                break;
+            case PAPER:
+                objName = "Papier";
+                break;
+        }
+       return objName;
     }
 
     private static void finalWin() {
 
         if (playerScore == compScore) {
-            System.out.println("Es wurde ein Unendschieden in der Gesamtwertung erreicht.");
+            System.out.println("Es wurde ein Unentschieden in der Gesamtwertung erreicht.");
         } else if (playerScore > compScore) {
             System.out.println("Spieler gewinnt in der Gesamtwertung.");
         } else {
             System.out.println("Computer gewinnt in der Gesamtwertung.");
         }
 
-        System.out.println("Gesamtscore: Spieler: " + playerScore + " Computer: " + compScore + " punkten");
-
+        System.out.println("Gesamtscore: Spieler: " + playerScore + " Computer: " + compScore + " Punkten");
+        System.out.println("Du hast den Computer insgesamt " + counter + " mal herrausgefordert.");
     }
 }
